01736f2b6532699c7810168a1b59d2d6e4e4bddf
ilyosdev <ilyosaxmadaliy@gmail.com>
2023-07-11 17:14:45 +0500

    //

--- index.js
+++ index.js
@@ -9,43 +9,43 @@ const app = express();
 app.use(express.json());
 app.use("/country", countryRouter);
 
-let data = [];
-try {
-  data = await axios("https://restcountries.com/v3.1/all").then((res) =>
-    res.data
-  );
-} catch (err) {
-  console.log(err.message);
-}
-data?.map((item) => {
-  const {
-    name,
-    capital,
-    currencies,
-    region,
-    subregion,
-    languages,
-    flags,
-    population,
-    borders,
-  } = item;
-  try {
-    Country.create({
-      name: name.common,
-      capital: capital ? capital[0] : null,
-      region: region,
-      subRegion: subregion,
-      flag: flags.png,
-      population: population,
-      nativeName: name?.nativeName && Object.values(name.nativeName)[0]?.common,
-      currency: currencies && Object.values(currencies)[0]?.name,
-      language: languages && Object.values(languages),
-      borderCountries: borders,
-    });
-  } catch (error) {
-    console.log(error);
-  }
-});
+// let data = [];
+// try {
+//   data = await axios("https://restcountries.com/v3.1/all").then((res) =>
+//     res.data
+//   );
+// } catch (err) {
+//   console.log(err.message);
+// }
+// data?.map((item) => {
+//   const {
+//     name,
+//     capital,
+//     currencies,
+//     region,
+//     subregion,
+//     languages,
+//     flags,
+//     population,
+//     borders,
+//   } = item;
+//   try {
+//     Country.create({
+//       name: name.common,
+//       capital: capital ? capital[0] : null,
+//       region: region,
+//       subRegion: subregion,
+//       flag: flags.png,
+//       population: population,
+//       nativeName: name?.nativeName && Object.values(name.nativeName)[0]?.common,
+//       currency: currencies && Object.values(currencies)[0]?.name,
+//       language: languages && Object.values(languages),
+//       borderCountries: borders,
+//     });
+//   } catch (error) {
+//     console.log(error);
+//   }
+// });
 
 let port = process.env.PORT || 5000;
 
