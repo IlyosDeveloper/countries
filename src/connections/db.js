570710f06f1ff946ba327aa363396baad9f73969
ilyosdev <ilyosaxmadaliy@gmail.com>
2023-07-11 16:57:46 +0500

    /

--- src/connections/db.js
+++ src/connections/db.js
@@ -1,9 +1,25 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize( 'countries', 'postgres', '12271996', {
-  host: 'localhost',
-  dialect: 'postgres'
-});
+
+  
+  const db = new Sequelize(
+    "postgres://countries_un6b_user:2rNX2KbEhT2S6oRZlxxpxE8dvvWDhFQw@dpg-cimjtrdgkuvgvh927en0-a.singapore-postgres.render.com/countries_un6b",
+  {
+    port: 5432,
+    dialect: "postgres",
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // <<<<<<< YOU NEED THIS
+      },
+    },
+  }
+);
+
+// const db = new Sequelize( 'countries', 'postgres', '12271996', {
+//   host: 'localhost',
+//   dialect: 'postgres'
+// });
 try {
   await db.authenticate();
   console.log("Successfully.");
